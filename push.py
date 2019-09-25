98603469b843e358722ae0873d8888941553ba83
Chirag Shah <chirag@srvmedia.com>
2019-09-25 10:43:14 +0530

    push file

---
+++ push.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from random import randint
+
+import json
+import redis
+from django.conf import settings
+from django.http import JsonResponse
+
+redis_cache = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DATABASE)
+
+
+def push(payload):
+    redis_cache_key = "dakiya_payload_" + str(int(datetime.timestamp(datetime.now()))) + str(randint(100, 999))
+    redis_cache.set(redis_cache_key, json.dumps(payload))
+    response = {'status': True, 'redis_cache_key': redis_cache_key}
+    return response
